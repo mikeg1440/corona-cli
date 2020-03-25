1b44122812f3300c786907ff055ddfda23a608c5
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-25 11:17:19 -0400

    Refactor theEnd.js file to use the exported infoCountries from display.js

--- utils/theEnd.js
+++ utils/theEnd.js
@@ -4,24 +4,6 @@ const chalk = require('chalk');
 const cyan = chalk.cyan;
 const dim = chalk.dim;
 
-const infoStates = () =>
-	console.log(
-		dim(`
-const infoCountries = () =>
-	console.log(
-		dim(`
-			\n${sym.info} ${cyan(`KEY:`)}
-${dim(`❯ `)}${cyan(`Country:`)} Name of the country
-${dim(`❯ `)}${cyan(`Cases:`)} Total number of cases in a country
-${dim(`❯ `)}${cyan(`Cases (today):`)} Cases in 24 hours GMT/UTC
-${dim(`❯ `)}${cyan(`Deaths:`)} Total number of deaths in a country
-${dim(`❯ `)}${cyan(`Deaths (today):`)} Deaths in 24 hours GMT/UTC
-${dim(`❯ `)}${cyan(`Recovered:`)} Total number of recovered people
-${dim(`❯ `)}${cyan(`Active:`)}  Total number of active patients
-${dim(`❯ `)}${cyan(`Critical:`)} Total number of critical patients
-${dim(`❯ `)}${cyan(`Per Million:`)} Affected patients per million
-`)
-	);
 
 module.exports = async (lastUpdated, states, minimal) => {
 	if (minimal) return console.log();
@@ -29,6 +11,8 @@ module.exports = async (lastUpdated, states, minimal) => {
 	states && infoStates();
 	!states && infoCountries();
 	states && display.infoStates();
+	!states && display.infoCountries();
+
 	console.log(
 		`\n${sym.success} ${dim(
 			`Star the repo for updates → https://git.io/corona-cli`
