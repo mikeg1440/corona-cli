0f6d60e2a690d72dc1b18b4012b96cc3b56ba855
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-25 11:14:19 -0400

    Move infoCountries function from theEnd.js to display.js as a exported function

--- utils/display.js
+++ utils/display.js
@@ -33,5 +33,25 @@ const infoStates = () =>
       ${dim(`❯ `)}${cyan(`Active:`)}  Total number of active patients
     `)
   );
+
+  const infoCountries = () =>
+  	console.log(
+  		dim(`
+  			\n${sym.info} ${cyan(`KEY:`)}
+        ${dim(`❯ `)}${cyan(`Country:`)} Name of the country
+        ${dim(`❯ `)}${cyan(`Cases:`)} Total number of cases in a country
+        ${dim(`❯ `)}${cyan(`Cases (today):`)} Cases in 24 hours GMT/UTC
+        ${dim(`❯ `)}${cyan(`Deaths:`)} Total number of deaths in a country
+        ${dim(`❯ `)}${cyan(`Deaths (today):`)} Deaths in 24 hours GMT/UTC
+        ${dim(`❯ `)}${cyan(`Recovered:`)} Total number of recovered people
+        ${dim(`❯ `)}${cyan(`Active:`)}  Total number of active patients
+        ${dim(`❯ `)}${cyan(`Critical:`)} Total number of critical patients
+        ${dim(`❯ `)}${cyan(`Per Million:`)} Affected patients per million
+      `)
+  	);
+
+
+
 exports.showTable = showTable
 exports.infoStates = infoStates
+exports.infoCountries = infoCountries
