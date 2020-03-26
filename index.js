31472cb2672406c5e970821e3bac371c559c17da
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-26 15:22:48 -0400

    Refactor index.js to seperate concerns

--- index.js
+++ index.js
@@ -71,46 +71,76 @@ const cyan = chalk.cyan;
     },
   ]
 
-  const thickBorder = { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
-         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
-         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
-         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+  // const thickBorder = { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+  //        , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+  //        , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+  //        , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
 
 
   states = false
+	input = undefined
+	if (input !== undefined){
+		renderData(country, states, xcolor, minimal, spinner, options)
+	}else {
+		inquirer.prompt(questions).then(async ans => {
+			console.log(ans)
+			if (ans.menu === 'usa'){
+				states = true
+				country = ans.menu
+			}else {
+				states = ans.state
+				country = ans.country
+			}
+			renderData(country, states, xcolor, minimal, spinner, options)
+			// console.log(`states: ${states}\ncountry: ${country}`)
 
-  inquirer.prompt(questions).then(async ans => {
-		console.log(ans)
-		if (ans.menu === 'usa'){
-			states = true
-			country = ans.menu
-		}else {
-			states = ans.state
-			country = ans.country
-		}
-
-    // console.log(`states: ${states}\ncountry: ${country}`)
-
-    const head = xcolor ? single : colored;
-
-  	const headStates = xcolor ? singleStates : coloredStates;
-  	const border = minimal ? borderless : {};
-  	const table = !states
-  		? new Table({ head, style, chars: thickBorder, colWidths: [10, 20] })
-  		: new Table({ head: headStates, style, chars: thickBorder, colWidths: [10, 20] });
-
-  	// Display data.
-  	spinner.start();
-  	const lastUpdated = await getWorldwide(table, states);
-    theEnd(lastUpdated, states, minimal);
-    console.log(`states: ${states}\ncountry: ${country}`)
-  	await getCountry(spinner, table, states, country).then(res => display.showTable(res));
-  	await getStates(spinner, table, states, options).then(res => display.showTable(res));
-  	await getCountries(spinner, table, states, country, options).then(res => display.showTable(res));
+			// const head = xcolor ? single : colored;
+			//
+			// const headStates = xcolor ? singleStates : coloredStates;
+			// const border = minimal ? borderless : {};
+			// const table = !states
+			// 	? new Table({ head, style, chars: thickBorder, colWidths: [10, 20] })
+			// 	: new Table({ head: headStates, style, chars: thickBorder, colWidths: [10, 20] });
+			//
+			// // Display data.
+			// spinner.start();
+			// const lastUpdated = await getWorldwide(table, states);
+			// theEnd(lastUpdated, states, minimal);
+			// console.log(`states: ${states}\ncountry: ${country}`)
+			// await getCountry(spinner, table, states, country).then(res => display.showTable(res));
+			// await getStates(spinner, table, states, options).then(res => display.showTable(res));
+			// await getCountries(spinner, table, states, country, options).then(res => display.showTable(res));
 
 
-  })
+		})
 
+	}
 
 
 })();
+
+const thickBorder = { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+			 , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+			 , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+			 , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+
+
+async function renderData(country, states, xcolor, minimal, spinner, options) {
+	const head = xcolor ? single : colored;
+
+	const headStates = xcolor ? singleStates : coloredStates;
+	const border = minimal ? borderless : {};
+	const table = !states
+		? new Table({ head, style, chars: thickBorder, colWidths: [10, 20] })
+		: new Table({ head: headStates, style, chars: thickBorder, colWidths: [10, 20] });
+
+	// Display data.
+	spinner.start();
+	const lastUpdated = await getWorldwide(table, states);
+	theEnd(lastUpdated, states, minimal);
+	console.log(`states: ${states}\ncountry: ${country}`)
+	await getCountry(spinner, table, states, country).then(res => display.showTable(res));
+	await getStates(spinner, table, states, options).then(res => display.showTable(res));
+	await getCountries(spinner, table, states, country, options).then(res => display.showTable(res));
+
+}
